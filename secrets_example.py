a9f5a999848db1d9025a711179d81e891534b415
Michael J <mmorel@Michaels-MacBook-Pro.local>
2018-10-06 03:21:11 -0700

    aegistov added basic script to query user by summoner id. Will retrieve their user data and match history. Prints custom string using data

---
+++ secrets_example.py
@@ -0,0 +1,7 @@
+"""
+    Visit https://developer.riotgames.com/ to locate your api key and replace the fake one with your key.
+    Remember to not replace this file. Instead, copy it and edit the api_key
+"""
+
+api_key = 'RGAPI-im-a-fake-api-key'
+
