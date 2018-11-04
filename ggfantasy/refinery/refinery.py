bcf27d20e5781664e67f2e743257a218c6d9daa1
Michael J <mmorel@Michaels-MacBook-Pro.local>
2018-11-03 23:15:28 -0700

    Cleaned up refinery

--- ggfantasy/refinery/refinery.py
+++ ggfantasy/refinery/refinery.py
@@ -1,6 +1,3 @@
-"""
-    Processor for Live Data from League of Legends websocket
-"""
 import websockets
 import requests
 import asyncio
@@ -13,60 +10,8 @@ from datetime import datetime
 from ggfantasy.cipher.cipher import Cipher
 from mockups.services.league_mock import LeagueMock
 
-live_stream = 'wss://livestats.proxy.lolesports.com/stats?jwt={}'
-# live_stream = 'ws://localhost:8765'
-our_websocket = 'ws://localhost:9734'
-jwt_generator = 'https://api.lolesports.com/api/issueToken'
 
-# "kills": 2,
-#  "deaths": 1,
-#  "assists": 7,
-#  "doubleKills": 0,
-#  "tripleKills": 0,
-#  "quadraKills": 0,
-#  "pentaKills": 0,
-# "mk": 233
-
-keys = ['kills', 'deaths', 'assists', 'doubleKills', 'tripleKills', 'quadraKills', 'pentaKills', 'mk']
-
-def get_token():
-    resp = requests.get(jwt_generator)
-    data = resp.json()
-    try:
-        print("Token: {}".format(data['token']))
-        return data['token']
-    except Exception:
-        raise Exception
-
-def process_event(event):
-    players = list()
-    try:
-        event_key = [*event][0]
-        players = event[event_key]['playerStats']
-        for player in players:
-            processed_player = {k: v for k, v in players[player].items() if k in keys}
-            players[player] = processed_player
-    except Exception as e:
-        pass
-    return players
-
-async def open_connection():
-    token = get_token()
-    async with websockets.connect(live_stream.format(token)) as websocket:
-    # async with websockets.connect(live_stream) as websocket:
-        # async for message in websocket:
-        while True:
-            data = await websocket.recv()
-            processed_data = process_event(json.loads(data))
-            async with websockets.connect(our_websocket) as gg_socket:
-                await gg_socket.send(json.dumps(processed_data))
-            print('Data: {}'.format(data))
-            print('Processed data: {}'.format(processed_data))
-            f1.write('{},\n'.format(data))
-            f2.write('{},\n'.format(json.dumps(processed_data)))
-
-
-# Should have a base refinery and have each game inherit from it
+# TODO Abstract this so each game inherits from base refinery
 class Refinery:
     """
         Refinery will receive and process live data, and broadcast processed data
@@ -76,6 +21,7 @@ class Refinery:
     LIVE_URL = 'wss://livestats.proxy.lolesports.com/stats?jwt={}'
     JWT_GEN = 'https://api.lolesports.com/api/issueToken'
 
+    # Desired keys when processing events
     KEYS = [
         'kills',
         'deaths',
@@ -96,13 +42,13 @@ class Refinery:
         asyncio.get_event_loop().run_until_complete(self.processor(dry_run))
         asyncio.get_event_loop().run_forever()
 
-
     def connect_to_db(self):
         now = datetime.now()
         self.raw_data_table = open('{}_raw_data.txt'.format(now), 'w')
         self.processed_data_table = open('{}_processed_data.txt'.format(now), 'w')
 
-    def get_path(self):
+    @staticmethod
+    def get_path():
         return os.path.dirname(os.path.abspath(__file__))
 
     def _get_token(self):
@@ -116,35 +62,32 @@ class Refinery:
     def _get_auth_live_stream_url(self):
         return self.LIVE_URL.format(self._get_token())
 
-    def process_event(self, event):
-        players = dict()
+    @staticmethod
+    def process_event(event):
         try:
             # Grabs the first key of the JSON programmatically
             event_key = [*event][0]
 
-            # Here we should be grabbing other events like team events
-            # Then send them into their own processors
+            # TODO Create other processors to process other events
+            # Other processors:
+            # Teams
+            # Game Initializer - grab champ picks, bans, etc.
             players = event[event_key]['playerStats']
             for player in players:
                 processed_player = {k: v for k, v in players[player].items() if k in keys}
                 players[player] = processed_player
-
         except KeyError as e:
             raise Exception(e)
         return players
 
     async def processor(self, dry_run=False):
-        # TODO Import LeagueMock
-        # TODO Create a Mock directory for different mock services
         source = self._get_auth_live_stream_url() if not dry_run else LeagueMock.get_url()
-        print("Source: {}".format(source))
         async with websockets.connect(source) as websocket:
-            # Make this NOT an infinite loop
+            # TODO Make this NOT an infinite loop
             # Possibly with <async for message in websocket>
             while True:
                 data = await websocket.recv()
-                print(data)
-                processed_data = process_event(json.loads(data))
+                processed_data = self.process_event(json.loads(data))
                 async with websockets.connect(Cipher.get_url()) as cipher:
                     await cipher.send(json.dumps(processed_data))
 
@@ -157,7 +100,7 @@ class Refinery:
 
 if __name__ == '__main__':
     try:
-        dry_run  = bool(sys.argv[1])
+        dry_run = bool(sys.argv[1])
     except IndexError:
         dry_run = False
     refinery = Refinery()
