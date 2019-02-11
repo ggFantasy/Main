ad4f61045b7be2c596ddf3bac8fe360d48aedbc3
Michael J <mmorel@Michaels-MacBook-Pro.local>
2019-02-10 19:10:18 -0800

    mmorel: forgot setup file

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
+        'flask-restful',
         'requests>=2.20.0',
         'crontab',
+        'python-socketio>=3.0',
+        'SQLAlchemy',
+        'mysqlclient',
     ]
 )
